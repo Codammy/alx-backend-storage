861597eb495d7e1436ea599efb4f1af403b32c2a
codam <damisco005@gmail.com>
2024-10-27 13:34:55 +0100

    made changes to 8-all.py

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -8,8 +8,6 @@ from typing import List, Dict
 
 def list_all(mongo_collection) -> List[Dict]:
     """lists all documents in a collection"""
-    client: MongoClient = MongoClient()
-    db = client.my_db
 
-    all_doc = db[mongo_collection].find().to_list()
+    all_doc = mongo_collection.find().to_list()
     return all_doc if len(all_doc) > 0 else []
