8546d3133b9bbb6148d062053e78db7a11199415
codam <damisco005@gmail.com>
2024-10-28 04:10:50 +0100

    new commit

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Python function that inserts a new document in a collection based on kwargs:
+Python function that inserts a new document in a collection based on kwargs
 """
 from pymongo.collection import Collection
 from typing import Any
@@ -9,4 +9,4 @@ from typing import Any
 def insert_school(mongo_collection: Collection, **kwargs: dict[Any, Any]) -> str:
     """inserts a new document in a collection based on kwargs"""
 
-    return mongo_collection.insert_one(kwargs).inserted_id    
+    return mongo_collection.insert_one(kwargs).inserted_id 
