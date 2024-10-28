6a28dac786d7206974cd18531f08d89ca9f3c350
codam <damisco005@gmail.com>
2024-10-28 05:22:20 +0100

    fixed

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -3,9 +3,10 @@
 Python function that changes all topics of a school document based on the name
 """
 from pymongo.collection import Collection
+from typing import List
 
 
-def update_topics(mongo_collection: Collection, name: str, topics: list[str]):
+def update_topics(mongo_collection: Collection, name: str, topics: List[str]):
     """changes all topics of a school document based on the name"""
 
     mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -4,11 +4,11 @@ Python function that
 inserts a new document in a collection based on kwargs
 """
 from pymongo.collection import Collection
-from typing import Any
+from typing import Any, Dict
 
 
 def insert_school(mongo_collection: Collection,
-                  **kwargs: dict[Any, Any]) -> str:
+                  **kwargs: Dict[Any, Any]) -> str:
     """inserts a new document in a collection based on kwargs"""
 
     return mongo_collection.insert_one(kwargs).inserted_id
