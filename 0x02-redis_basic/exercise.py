1fb30bb2f45be169c11bfc2aa57805da72cc1764
Oguntola Damilola <damilolaabdulmolik@gmail.com>
2024-12-17 00:42:50 +0100

    corrected exercise file

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -14,7 +14,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: str | bytes | int | float) -> str:
+    def store(self, data: any) -> str:
         """method for retrieval and storage of data"""
         r_key = str(uuid.uuid4())
         self._redis.set(r_key, data)
