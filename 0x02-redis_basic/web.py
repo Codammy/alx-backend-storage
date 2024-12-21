adb3a847ace212621c12ad16f0e8da8c960b7f46
Oguntola Damilola <damilolaabdulmolik@gmail.com>
2024-12-21 08:43:46 +0100

    redis on the web

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" In this tasks, i will implement a
+get_page function (prototype: def get_page(url: str) -> str:).
+The core of the function is very simple.
+It uses the requests module to obtain the HTML content of a
+particular URL and returns it.
+"""
+import requests
+import redis
+import typing
+from functools import wraps
+
+
+def cache_count(func: typing.Callable) -> typing.Callable:
+    """decorator function for caching url counts"""
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        """wrapper function"""
+        r = redis.Redis()
+        url = args[0]
+        r.incr(f"count:{{url}}")
+        r.expire(url, 10)
+        return get_page(url)
+    return wrapper
+
+
+@cache_count
+def get_page(url: str) -> str:
+    resp = requests.get(url)
+    return resp.text
